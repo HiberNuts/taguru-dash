f215e5841ee546e98fd80c5dba718f1cc6daf5ca
Raghav Jindal <raghavjindal0212@gmail.com>
2023-05-06 01:00:14 +0530

    added landing

--- src/routes/AuthenticationRoutes.js
+++ src/routes/AuthenticationRoutes.js
@@ -1,44 +1,45 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 // project imports
-import Loadable from 'ui-component/Loadable';
-import MinimalLayout from 'layout/MinimalLayout';
-import Signup from 'register/Signup';
-import Login from 'register/Login';
+import Loadable from "ui-component/Loadable";
+import MinimalLayout from "layout/MinimalLayout";
+import Signup from "register/Signup";
+import Login from "register/Login";
+import Landing from "landing/Landing";
 
 // login option 3 routing
-const AuthLogin3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Login3')));
-const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
+const AuthLogin3 = Loadable(lazy(() => import("views/pages/authentication/authentication3/Login3")));
+const AuthRegister3 = Loadable(lazy(() => import("views/pages/authentication/authentication3/Register3")));
 
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
 const AuthenticationRoutes = {
-    path: '/',
-    element: <MinimalLayout />,
-    children: [
-        {
-            path: '/pages/login/login3',
-            element: <AuthLogin3 />
-        },
-        {
-            path: '/pages/register/register3',
-            element: <AuthRegister3 />
-        },
-        // {
-        //     path: '/',
-        //     element: <HomeLadningPage />
-        // },
+  path: "/",
+  element: <MinimalLayout />,
+  children: [
+    {
+      path: "/pages/login/login3",
+      element: <AuthLogin3 />,
+    },
+    {
+      path: "/pages/register/register3",
+      element: <AuthRegister3 />,
+    },
+    {
+      path: "/land",
+      element: <Landing />,
+    },
 
-        // {
-        //     path: '/signup',
-        //     element: <Signup />
-        // },
-        
-        // {
-        //     path: '/login',
-        //     element: <Login />
-        // },
-    ]
+    // {
+    //     path: '/signup',
+    //     element: <Signup />
+    // },
+
+    // {
+    //     path: '/login',
+    //     element: <Login />
+    // },
+  ],
 };
 
 export default AuthenticationRoutes;
